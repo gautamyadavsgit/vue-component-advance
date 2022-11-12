2c95db48d27642ffc76f6afd70775f2bb0d0adfc
gautamyadavsgit <gautaminit@gmail.com>
2022-11-12 08:34:42 +0530

    changes saved

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
+      <section>
       <header>
         <slot name="header"></slot>
       </header>
-     <slot></slot>        
+     <slot></slot>
+    </section>        
     </div>
 </template>
 
@@ -19,5 +21,10 @@ div {
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
+}
+section header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -19,9 +19,5 @@ export default {
 <style scoped>
 
 
-section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+
 </style>
